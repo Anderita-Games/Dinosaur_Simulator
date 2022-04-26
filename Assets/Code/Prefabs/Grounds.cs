298148131ed14caef62483815c1d8677138ee1ba
Andrew Friedman <andrewfriedman01@gmail.com>
2022-04-25 23:53:19 -0400

    Add demo

--- Assets/Code/Prefabs/Grounds.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Grounds : MonoBehaviour {
-	void Update () {
-		if (this.gameObject.transform.position.x < GameObject.Find("Player").transform.position.x - 60 || GameObject.Find("Canvas").GetComponent<Gamemaster>().Game_State == "Restart") {
-			Destroy(this.gameObject);
-		}
-	}
-}
